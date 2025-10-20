9b4f3397b27cc9175f62c8a3169e1f1b36a7515c
Erly123 <choquelayme@gmail.com>
2025-10-20 18:22:50 -0300

    cambio en application

--- sw-planet-api/src/main/java/com/example/swplanetapi/SwPlanetApiApplication.java
+++ sw-planet-api/src/main/java/com/example/swplanetapi/SwPlanetApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.sw_planet_api;
+package com.example.swplanetapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
